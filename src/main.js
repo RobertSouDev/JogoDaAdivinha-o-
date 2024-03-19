667fd74d163d6e9536fa269645cf790cb7c764f0
RobertSouDev <rogerhallks1@gmail.com>
2024-03-19 17:39:54 -0300

    adicionei as funcoes call beck

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const randomNumber = Math.round(Math.random()* 10)
 let xAttempts = 1
 
 
-function handleClick(event){
-    event.preventDefault()
+function handleTryClick(event){
+    event.preventDefault() // nao faca o padrao desse evento
 
     let inputNumber = document.querySelector('#inputNumber')
 
@@ -15,6 +15,7 @@ function handleClick(event){
         document.querySelector(".screen2 h2").innerHTML =  "voce acertou em "+ xAttempts + " tentativas"
     }
     console.info(xAttempts)
+    inputNumber.value = ""
     xAttempts++
 }
 
@@ -22,3 +23,11 @@ function volta(){
     document.querySelector(".screen1").classList.remove("hide")
     document.querySelector(".screen2").classList.add("hide")
 }
+
+// Eventos
+
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+btnTry.addEventListener('click',handleTryClick)
+btnReset.addEventListener('click', volta)
